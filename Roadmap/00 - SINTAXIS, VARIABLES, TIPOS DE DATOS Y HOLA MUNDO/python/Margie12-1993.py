2d351d8a25e5ae55bcff39a85e3759e8d3720ffa
Margie12-1993 <duhast0212@gmail.com>
2025-03-19 20:12:05 -0500

    Create Margie12-1993.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Margie12-1993.py
@@ -0,0 +1,12 @@
+# https://www.python.org/
+
+# comentario en 1 linea
+
+¨¨¨Esto es un comentario
+en varias
+lineas¨¨¨
+
+
+my_variable = ¨mi variable¨
+my_variable = ¨dar nuevo valor a mi variable¨
+
